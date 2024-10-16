411a315a35f3a25c28ff8229bb576f50f1774caf
nkoichev <nikolay.koychev@gmail.com>
2024-10-16 18:34:30 +0300

    mac

--- app.py
+++ app.py
@@ -48,3 +48,43 @@ st.write("""
 """)
 
 
+st.write("---")
+
+st.code("∃s⊆S:∀v∈V:D(s,v)")
+
+# Дефинираме множествата S и V
+elements = {1, 2, 3, 4, 5}
+values = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+S = st.multiselect("Choose some elements of S:", elements)
+V = st.multiselect("Choose some values of V:", values)
+
+
+# Дефинираме предиката D(s, v), който проверява дали v е в s
+def D(s, v):
+    return v in s
+
+
+st.code("def D(s, v): return v in s")
+st.write("В тази функция сме заложили предиката D(s, v), която проверява дали стойността v е в множеството s")
+
+# Проверяваме дали съществува подмножество s от S, което удовлетворява условията
+def find_subset(S, V):
+    # Намираме всички подмножества на S
+    from itertools import chain, combinations
+    
+    def all_subsets(S):
+        return chain.from_iterable(combinations(S, r) for r in range(len(S)+1))
+
+    for s in all_subsets(S):
+        # Проверяваме дали за всяко v от V, D(s, v) е вярно
+        if all(D(s, v) for v in V):
+            return set(s)
+    return None
+
+# Извеждаме резултата
+subset = find_subset(S, V)
+if subset is not None:
+    st.write(f"Резултат: Съществува подмножество s = {subset}, за което ∀v ∈ V: D(s, v) е **:green[вярно]**")
+else:
+    st.write("Резултат: **:red[Няма такова подмножество]**")
