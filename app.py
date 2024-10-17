b08276eaa69f3c4c07855df5667d670da1e1475e
nkoichev <nikolay.koychev@gmail.com>
2024-10-17 05:20:49 +0300

    ...

--- app.py
+++ app.py
@@ -92,9 +92,9 @@ if choose == 'Лекция 1':
     code_implication = st.code("A → B = ¬A ∨ B")
 
     st.write(f"""
-                :blue[∧] represents Conjunction (AND) <br>
-                :blue[∨] represents Disjunction (OR) <br>
-                :blue[¬] represents Negation (NOT) <br>
+                :blue[∧] represents Conjunction (AND) <br>  
+                :blue[∨] represents Disjunction (OR) <br>  
+                :blue[¬] represents Negation (NOT) <br>  
                 :blue[→] represents Implication (IF THEN)
                     """)
 
